16dc9c719616b379fb60ed5ea18f2a3c05387282
hqv <hannahqvalentine@gmail.com>
2023-06-14 19:36:52 -0500

    floor to avoid floating point quantites of gold

--- files/scripts/gold_decay.lua
+++ files/scripts/gold_decay.lua
@@ -4,5 +4,5 @@ dofile_once( "mods/gold_decay/files/scripts/helpers.lua" )
 local entity = GetUpdatedEntityID();
 local x, y = EntityGetTransform( entity );
 local multiplier = ModSettingGet("gold_decay.multiplier")
-local goldValue = getInternalVariableValue( entity, "gold_value", "value_int" ) * multiplier / 100
+local goldValue = math.floor(getInternalVariableValue( entity, "gold_value", "value_int" ) * multiplier / 100)
 GameCreateParticle( "gold", x, y, goldValue, 0, 0, true );
